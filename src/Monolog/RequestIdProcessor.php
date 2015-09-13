8a0eab3cfaa2ccbee1903fdfdf940f9613a35fd2
Christopher Davis <cdavis9999@gmail.com>
2015-09-13 13:22:29 -0400

    Introduce the Monolog Processor
    Adds the request ID to the messages if present.

---
+++ src/Monolog/RequestIdProcessor.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of chrisguitarguy/request-id-bundle
+
+ * Copyright (c) Christopher Davis <http://christopherdavis.me>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Chrisguitarguy\RequestId\Monolog;
+
+use Chrisguitarguy\RequestId\RequestIdStorage;
+
+/**
+ * Adds the request ID to the Monolog record's `extra` key so it can be used
+ * in formatters, etc.
+ *
+ * @since   1.0
+ */
+final class RequestIdProcessor
+{
+    /**
+     * @var RequestIdStorage
+     */
+    private $idStorage;
+
+    public function __construct(RequestIdStorage $storage)
+    {
+        $this->idStorage = $storage;
+    }
+
+    public function __invoke(array $record)
+    {
+        if ($id = $this->idStorage->getRequestId()) {
+            $record['extra']['request_id'] = $id;
+        }
+
+        return $record;
+    }
+}
---
+++ test/unit/Monolog/RequestIdProcessorTest.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of chrisguitarguy/request-id-bundle
+
+ * Copyright (c) Christopher Davis <http://christopherdavis.me>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Chrisguitarguy\RequestId\Monolog;
+
+use Chrisguitarguy\RequestId\RequestIdStorage;
+use Chrisguitarguy\RequestId\UnitTestCase;
+
+class RequestIdProcessorTest extends UnitTestCase
+{
+    private $idStorage, $processor;
+
+    public function testProcessorDoesNotSetRequestIdWhenNoIdIsPresent()
+    {
+        $this->withRequestId(null);
+
+        $record = call_user_func($this->processor, ['extra' => []]);
+
+        $this->assertArrayNotHasKey('request_id', $record['extra']);
+    }
+
+    public function testProcessorAddsRequestIdWhenIdIsPresent()
+    {
+        $this->withRequestId('abc123');
+
+        $record = call_user_func($this->processor, ['extra' => []]);
+
+        $this->assertArrayHasKey('request_id', $record['extra']);
+        $this->assertEquals('abc123', $record['extra']['request_id']);
+    }
+
+    protected function setUp()
+    {
+        $this->idStorage = $this->getMock(RequestIdStorage::class);
+        $this->processor = new RequestIdProcessor($this->idStorage);
+    }
+
+    private function withRequestId($id)
+    {
+        $this->idStorage->expects($this->once())
+            ->method('getRequestId')
+            ->willReturn($id);
+    }
+}
