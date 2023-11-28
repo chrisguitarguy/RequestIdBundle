d6825aa105122681fb6f44323d7e93ff83015eba
Frank Dekker <fdekker@123inkt.nl>
2023-11-28 20:12:38 +0100

    Cleanup

--- tests/Unit/Generator/SymfonyUuid4GeneratorTest.php
+++ tests/Unit/Generator/SymfonyUuid4GeneratorTest.php
@@ -1,14 +1,6 @@
 <?php
-/*
- * This file is part of chrisguitarguy/request-id-bundle
 
- * Copyright (c) Christopher Davis <http://christopherdavis.me>
- *
- * For full copyright information see the LICENSE file distributed
- * with this source code.
- *
- * @license     http://opensource.org/licenses/MIT MIT
- */
+declare(strict_types=1);
 
 namespace DR\SymfonyRequestId\Tests\Unit\Generator;
 
